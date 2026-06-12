4ecdfde29eec578321ab4cc4facb9957e81816c9
SpandanaAithagoni <aithagonispandana@gmail.com>
2026-06-12 14:19:13 +0530

    Update customer_service.py

--- Retail-Inventory-Order-Management-System-Core-Python/src/services/customer_service.py
+++ Retail-Inventory-Order-Management-System-Core-Python/src/services/customer_service.py
@@ -1,31 +1,74 @@
 # src/services/customer_service.py
 
-from src.dao import customer_dao
+from src.dao.customer_dao import CustomerDAO
+
 
 class CustomerError(Exception):
     pass
 
-class CustomerService:
-    def __init__(self):
-        self.dao = customer_dao
 
-    def add_customer(self, name, email, phone, city=None):
+class CustomerService:
+
+    def __init__(self):
+        self.dao = CustomerDAO()
+
+    def add_customer(
+        self,
+        name,
+        email,
+        phone,
+        city=None
+    ):
         try:
-            return self.dao.add_customer(name, email, phone, city)
+            return self.dao.create_customer(
+                name,
+                email,
+                phone,
+                city
+            )
         except Exception as e:
             raise CustomerError(str(e))
 
     def list_customers(self):
         return self.dao.list_customers()
 
-    def update_customer(self, cust_id, phone=None, city=None):
+    def update_customer(
+        self,
+        cust_id,
+        phone=None,
+        city=None
+    ):
         try:
-            return self.dao.update_customer(cust_id, phone, city)
+            return self.dao.update_customer(
+                cust_id,
+                phone,
+                city
+            )
         except Exception as e:
             raise CustomerError(str(e))
 
-    def delete_customer(self, cust_id):
+    def delete_customer(
+        self,
+        cust_id
+    ):
         try:
-            return self.dao.delete_customer(cust_id)
+            return self.dao.delete_customer(
+                cust_id
+            )
         except Exception as e:
             raise CustomerError(str(e))
+
+    def get_customer(
+        self,
+        cust_id
+    ):
+        customer = self.dao.get_customer_by_id(
+            cust_id
+        )
+
+        if not customer:
+            raise CustomerError(
+                "Customer not found"
+            )
+
+        return customer
